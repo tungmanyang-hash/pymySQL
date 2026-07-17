da0ac7b8ab678faa0600148f41123cffa459031d
Chrisy <tungmanyang@gmail.com>
2026-07-18 00:36:53 +0800

    0717home

--- Chapter1/sql_utils.py
+++ Chapter1/sql_utils.py
@@ -1,3 +1,5 @@
+# 把這些打包成method
+
 # 引入所需套件
 import pymysql
 from configparser import ConfigParser
@@ -7,10 +9,11 @@ config.read('config.ini')
 
 
 class SqlUtils:
-    db_query = "SHOW DATABASES;"
+    db_query = "SHOW DATABASES;" # 把查詢的語句，寫在類別屬性裡面。有固定或是常用的語句，都可以寫在類別屬性裡。會變得很方便，不用一直重複打
 
 
-    def __init__(self, database=None):
+    # 基於連線，才可以坐下面的操作。所以將 connection 寫在實例屬性，讓下面的 method 都可以用 self.connection 來取用這個實例屬性，做一些我們想做的操作
+    def __init__(self, database=None): # 我沒給指定 database 時，就預設是 None
         self.connection = pymysql.connect(
             host=config['DB']['host'],
             user=config['DB']['user'],
@@ -19,6 +22,9 @@ class SqlUtils:
             cursorclass=pymysql.cursors.DictCursor,
             database=database
     )
+        # connection 如果寫在全域變數，要同時查詢多筆資料時，會發生衝突，因此寫在 function 裡比較好
+
+
     
     def sql_query(self, query):
     # 建立 function 執行 SQL 查詢
@@ -28,7 +34,10 @@ class SqlUtils:
 
         return result
 
-    def show_dbs(self):
+
+    # 以下其實可以刪除了，因為已經把查詢寫成類別屬性打包進 sql_query 了
+    #  db_query = "SHOW DATABASES;" --> 12行的這句，其實就是下面的簡寫了
+    
     # 建立 function 執行 SQL 查詢
         with self.connection.cursor() as cursor:
             cursor.execute("SHOW DATABASES;")
